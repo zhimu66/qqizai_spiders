40016d68774e42eb91a2ce74854b09e9778bd5e7
breeze <a1319973331@163.com>
2021-06-29 18:45:29 +0800

    https://github.com/selfshore/spiders

--- 企业公示系统/run.py
+++ 企业公示系统/run.py
@@ -16,9 +16,14 @@ headers = {
 }
 r = requests.get('http://www.gsxt.gov.cn/index.html', headers=headers)
 cookie = dict_from_cookiejar(r.cookies)
+js = 'function get_jsl(){document={};' + re.findall('<script>(.*?)location', r.text)[0] + 'return document.cookie}'
+ctx = execjs.compile(js)
+jsl_ck = ctx.call('get_jsl')
+cookie.update({'__jsl_clearance': jsl_ck.split('=', 1)[1].split(';', 1)[0]})
+r = requests.get('http://www.gsxt.gov.cn/index.html', headers=headers, cookies=cookie)
 js = 'function jsl(){window = {navigator: {userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Geck' \
      'o) Chrome/83.0.4103.116 Safari/537.36"},outerWidth: 1920,outerHeight: 1050,};location = {reload:function () {' \
-     '}};document = {};' + re.findall('<script>(.*?)</script>', r.text)[0] + ';return document.cookie}'
+     '}};document = {};setTimeout = function (x) {x()};' + re.findall('<script>(.*?)</script>', r.text)[0] + ';return document.cookie}'
 ctx = execjs.compile(js)
 jsl_cookie = ctx.call('jsl')
 jsl_cookie = re.findall('=(.*?);', jsl_cookie)[0]
